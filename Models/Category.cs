38fcded1d1258769d11d486c55d86e10f17916af
Emre <emre.bas@ogr.bandirma.edu.tr>
2024-08-04 16:28:47 +0300

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace FormsApp.Models
+{
+    public class Category
+    {
+        public int categoryId { get; set; }
+        public string categoryName { get; set; } = string.Empty;
+    }
+}
